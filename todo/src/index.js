47da0187170adae5f40c8897d55b97a3bdd7b18e
Eric Daniels <eric@erdaniels.com>
2016-09-26 15:26:52 -0400

    Some cruft cleanup. todo_app -> todo-app

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MongoClient } from 'baas';
 
-let client = new MongoClient("todo", "http://localhost:8080/v1/app/todo_app")
+let client = new MongoClient("todo", "http://localhost:8080/v1/app/todo-app")
 
 function TodoItem({item=null, checkHandler=null}){
   let itemClass = item.checked ? "done" : "";
