260baa8a548972e0e6aa1553830251bf010703d5
Mike O'Brien <mpobrien005@gmail.com>
2016-10-19 08:34:44 -0400

    planner app updates; add/remove list, add/edit card, modals

---
+++ planner/src/home.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import {Link} from 'react-router'
+import AuthControls from './auth.js'
+
+var Home = React.createClass({
+  getInitialState: function(){
+    return {authed:this.props.route.client.auth() != null}
+  },
+  render:function(){
+    return (
+      <div>
+        {this.state.authed ? (<BoardListing db={this.props.route.db}/>) : <AuthControls client={this.props.route.client}/> }
+      </div>
+    )
+  }
+})
+
+let BoardAdder = React.createClass({
+  getInitialState:function(){
+    return {adding:false}
+  },
+  setup: function(){
+    this.setState({adding:true})
+  },
+  cancel: function(){
+    this.setState({adding:false})
+  },
+  save: function(){
+    if(this._name.value.length == 0 )
+      return
+    boards.insert([{"name":this._name.value}]).then(
+      ()=>{
+        this._name.value = ""
+        this.setState({adding:false})
+        this.props.onUpdate()
+      })
+  },
+  keydown: function(e){
+    if(e.keyCode == 13){
+      this.save()
+    } else if(e.keyCode == 27){
+      this.cancel()
+    }
+  },
+  render:function(){
+    if(!this.state.adding){
+      return (<button className="newboard" onClick={this.setup}>+ New Board</button>)
+    }else{
+      return (
+        <div>
+          <input type="text" placeholder="name" ref={(n)=>{this._name=n}} onKeyDown={this.keydown}/>
+          <button onClick={this.cancel}>Cancel</button>
+          <button onClick={this.save} ref={(n)=>{this._save=n}}>Save</button>
+        </div>
+      )
+    }
+  }
+})
+
+let BoardListing = React.createClass({
+  getInitialState: function(){
+    return {boards:[]}
+  },
+  componentWillMount: function(){
+    this.loadBoards()
+  },
+  loadBoards: function(){
+    this.props.db.boards.find({}, null).then((data)=>{this.setState({boards:data.result})})
+  },
+  render:function(){
+    return (
+      <div>
+        <ul className="boards">
+          { 
+            this.state.boards.map(
+              (x)=>{
+                return <BoardItem data={x} key={x._id["$oid"]} onUpdate={this.loadBoards}/>
+              }
+            )
+          }
+        </ul>
+        <BoardAdder onUpdate={this.loadBoards}/>
+      </div>
+    )
+  }
+})
+
+let BoardItem = React.createClass({
+  remove: function(){
+    if(confirm(`ey you sure you wanna delete '${this.props.data.name}'?`)){
+      boards.remove({_id:this.props.data._id}).then(this.props.onUpdate)
+    }
+  },
+  render:function(){
+    return (
+      <div className="board">
+        <span className="name">
+          <Link to={"/boards/" + this.props.data._id.$oid}>{this.props.data.name}</Link>
+        </span>
+        <button className="delete" onClick={this.remove}>X</button>
+      </div>
+    )
+  }
+})
+
+export {Home};
--- planner/src/index.js
+++ planner/src/index.js
@@ -1,119 +1,296 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {BaasClient, MongoClient} from 'baas';
-import {browserHistory, Router, Route , Link} from 'react-router'
+import {browserHistory, Router, Route, Link} from 'react-router'
 import AuthControls from "./auth.js"
+import {Home} from "./home.js"
+import Modal from "react-modal"
+import ObjectID from "bson-objectid";
 
 require("../static/planner.scss")
 
+import {Converter} from 'showdown';
+
+
+let modalStyle = {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(0, 0, 0, 0.75)'
+  },
+  content : {
+    position                   : 'absolute',
+    top                        : '40px',
+    maxWidth                   : '60%',
+    marginLeft                 : "auto",
+    marginRight                : "auto",
+    left                       : '40px',
+    right                      : '40px',
+    bottom                     : '40px',
+    border                     : '1px solid #ccc',
+    background                 : '#fff',
+    overflow                   : 'auto',
+    WebkitOverflowScrolling    : 'touch',
+    borderRadius               : '4px',
+    outline                    : 'none',
+    padding                    : '20px'
+  }
+}
+
+/*
+{
+  "_id": ObjectId("58069770ad7dd59e8710d069"),
+  "name": "Personal",
+  "owner_id": ObjectId("58069770772e2e772a073c99"),
+  "lists": {
+    "todo": {
+      "count": 2,
+      "cards": {
+        "58069770772e2e8921c99645": {
+          "_id": ObjectId("58069770772e2e8921c99645"),
+          "idx": 1,
+          "text": "hello"
+        },
+        "58069770772e2e8921c99646": {
+          "text": "it's me",
+          "_id": ObjectId("58069770772e2e8921c99646"),
+          "idx": 0
+        }
+      }
+    }
+  }
+}
+*/
+
 let baasClient = new BaasClient("http://localhost:8080/v1/app/planner")
-let db = new MongoClient(baasClient, "mdb1").getDb("planner")
-let boards = db.getCollection("boards")
-let lists = db.getCollection("lists")
-let cards = db.getCollection("cards")
-let members = db.getCollection("members")
+let rootDb = new MongoClient(baasClient, "mdb1").getDb("planner")
+let db = {
+  boards: rootDb.getCollection("boards"),
+  cards : rootDb.getCollection("cards"),
+  members : rootDb.getCollection("members"),
+}
 
-var Home = React.createClass({
-  getInitialState: function(){
-    return {authed:baasClient.auth() != null}
-  },
-  render:function(){
-    return (
+let Boards = function(props){
+  return (
+    <div>
       <div>
-        {this.state.authed ? (<Boards/>) : <AuthControls client={baasClient}/> }
+        {props.children}
       </div>
-    )
-  }
-})
-
-let BoardAdder = React.createClass({
-  getInitialState:function(){
-    return {adding:false}
-  },
-  setup: function(){
-    this.setState({adding:true})
-  },
-  cancel: function(){
-    this.setState({adding:false})
-  },
-  save: function(){
-    if(this._name.value.length == 0 )
-      return
-    boards.insert([{"name":this._name.value}]).then(
-      ()=>{
-        this._name.value = ""
-        this.setState({adding:false})
-        this.props.onUpdate()
-      })
-  },
-  keydown: function(e){
-    if(e.keyCode == 13){
-      this.save()
-    } else if(e.keyCode == 27){
-      this.cancel()
-    }
-  },
-  render:function(){
-    if(!this.state.adding){
-      return (<button className="newboard" onClick={this.setup}>+ New Board</button>)
-    }else{
-      return (
-        <div>
-          <input type="text" placeholder="name" ref={(n)=>{console.log("called!");this._name=n}} onKeyDown={this.keydown}/>
-          <button onClick={this.cancel}>Cancel</button>
-          <button onClick={this.save} ref={(n)=>{this._save=n}}>Save</button>
-        </div>
-      )
-    }
-  }
-})
-
-let Boards = React.createClass({
-  getInitialState: function(){
-    return {boards:[]}
-  },
-  componentWillMount: function(){
-    this.loadBoards()
-  },
-  loadBoards: function(){
-    boards.find({}, null).then((data)=>{this.setState({boards:data.result})})
-  },
-  render:function(){
-    return (
-      <div>
-        <ul className="boards">
-          { 
-            this.state.boards.map(
-              (x)=>{
-                return <Board data={x} key={x._id["$oid"]} onUpdate={this.loadBoards}/>
-              })
-          }
-        </ul>
-        <BoardAdder onUpdate={this.loadBoards}/>
-      </div>
-    )
-  }
-})
+    </div>
+  )
+}
 
 let Board = React.createClass({
-  remove: function(){
-    if(confirm(`you sure you wanna delete board ${this.props.data.name}?`)){
-      boards.remove({_id:this.props.data._id}).then(this.props.onUpdate)
-    }
+  getInitialState:function(){
+    return {board:{name:"", lists:{}}}
   },
-  /*load:function(){
-    boards.find({_id:{$oid:this.props.id}}, null).then((data)=>{this.setState({data:data[0]})})
+  load: function(){
+    this.props.route.db.boards.find({_id:{$oid:this.props.routeParams.id}}, null).then(
+      (data)=>{this.setState({board:data.result[0]})}
+    )
   },
-  componentWillMount:function(){
+  componentWillMount: function(){
     this.load()
   },
-  */
+  newList: function(){
+    this.setState({newList:true})
+  },
+  componentDidUpdate: function(){
+    if(this._newlistname){
+      this._newlistname.focus()
+    }
+  },
+  newListKeyDown: function(e){
+    if(e.keyCode == 13){
+      let name = this._newlistname.value
+      if(name.length > 0){
+        let setObj = {}
+        setObj["lists." + name] =  {"name":name, "cards":{}}
+        this.props.route.db.boards.update(
+          {_id:{$oid:this.props.routeParams.id}},
+          {$set:setObj}, false, false)
+        .then(()=>{
+          this._newlistname.value = ""
+          this.load()
+        })
+      }
+    } else if(e.keyCode == 27){
+      this.setState({newList:false})
+    }
+  },
   render:function(){
-    console.log(this.props)
+    let listKeys = Object.keys(this.state.board.lists)
     return (
       <div className="board">
-        <span className="name">{this.props.data.name}</span>
-        <button className="delete" onClick={this.remove}>X</button>
+        <h3>{this.state.board.name}</h3>
+
+        <div className="lists">
+          { listKeys.map((x)=> {
+              let v = this.state.board.lists[x];
+              return <List onUpdate={this.load} boardId={this.props.routeParams.id} db={this.props.route.db} key={x} name={x} data={v}/>
+             })
+          }
+          { this.state.newList ?
+            <div className="list">
+              <input type="textbox" ref={(n)=>{this._newlistname=n}} onKeyDown={this.newListKeyDown}></input>
+            </div>
+            :
+            <div className="new-list">
+              <button onClick={this.newList}>+ New List</button>
+            </div>
+          }
+        </div>
+      </div>
+    )
+  }
+})
+
+let Card = React.createClass({
+  getInitialState: function(){
+    return {hovered:false}
+  },
+  hoverIn:function(){
+    this.setState({hovered:true})
+  },
+  hoverOut:function(){
+    this.setState({hovered:false})
+  },
+  openEdit:function(){
+    this.props.openEdit(this.props.data._id)
+  },
+  render:function(){
+    return (
+      <div
+        className={"card-in-list" + (this.state.hovered ? " hovered" : "")}
+        onMouseOver={this.hoverIn} 
+        onMouseOut={this.hoverOut}
+        onClick={this.openEdit}>
+        <span className="summary">{this.props.data.summary}</span>
+        <span className="edit">edit</span>
+      </div>
+    )
+  }
+})
+
+
+let List = React.createClass({
+  getInitialState:function(){
+    return {showNewCardBox:false}
+  },
+  componentDidUpdate: function(){
+    if(this._newcard){
+      this._newcard.focus()
+    }
+  },
+  createNewCard : function(summary){
+    let db = this.props.db
+    let boardId = this.props.boardId
+    let listName = this.props.name
+    let oid = ObjectID().toHexString()
+    return db.cards.insert([{_id:{$oid:oid}, summary:summary}]).then(()=>{
+      let setObj = {}
+      setObj["lists."+listName+".cards."+oid] = {_id:{$oid:oid}, summary:summary}
+      db.boards.update(
+        {_id:{$oid:boardId}},
+        {$set:setObj}, false, false)
+    }).then(
+      this.setState({showNewCardBox:false})
+    )
+  },
+  newCardKeyDown: function(e){
+    if(e.keyCode == 13){
+      let summary = this._newcard.value;
+      if(summary.length > 0){
+        this.createNewCard(summary).then(this.props.onUpdate)
+      }
+    } else if(e.keyCode == 27) {
+      this.setState({newList:false})
+    }
+  },
+	quickAddCard: function(){
+		this.setState({showNewCardBox:true})
+	},
+  delete:function(){
+    let unsetObj = {}
+    unsetObj["lists." + this.props.name] = 1;
+    this.props.db.boards.update(
+      {_id:{$oid:this.props.boardId}},
+      {$unset:unsetObj}, false, false)
+    .then(this.props.onUpdate)
+  },
+  openEditor:function(cid){
+    this.setState({modalOpen:true, editingId: cid})
+  },
+  onCloseReq:function(){
+    this.setState({modalOpen:false})
+  },
+  render:function(){
+    return (
+      <div className="list">
+        <h4>{this.props.name}<button onClick={this.delete}>X</button></h4>
+        <div>
+          { 
+            Object.keys(this.props.data.cards).map((x) => {
+              let c = this.props.data.cards[x]
+              let cid = c._id
+              return <Card data={c} key={c._id.$oid}
+                openEdit={()=>{this.openEditor(cid)}}/>
+            })
+          }
+          { this.state.showNewCardBox ? 
+            <input type="textbox" placeholder="summary" ref={(n)=>{this._newcard=n}} onKeyDown={this.newCardKeyDown}/>
+           : null}
+        </div>
+        <Modal style={modalStyle} isOpen={this.state.modalOpen} onRequestClose={this.onCloseReq}>
+          <CardEditor db={this.props.db} listName={this.props.name} boardId={this.props.boardId} editingId={this.state.editingId} onUpdate={this.props.onUpdate}/>
+        </Modal>
+        <button onClick={this.quickAddCard}>Add card...</button>
+      </div>
+    )
+  }
+})
+
+let CardEditor = React.createClass({
+  save: function(){
+    let newSummary = this._summary.value;
+    this.props.db.cards.update({_id:this.props.editingId}, 
+      {$set:{summary:newSummary, description:this._desc.value}},
+      false, false)
+    .then(()=>{
+      let setObj = {}
+      setObj[`lists.${this.props.listName}.cards.${this.props.editingId.$oid}.summary`] = newSummary;
+      return this.props.db.boards.update({_id:{$oid:this.props.boardId}}, {$set:setObj}, false, false)
+    })
+    .then(this.props.onUpdate)
+  },
+  getInitialState:function(){
+    return {data:{summary:"", description:""}}
+  },
+  componentWillMount:function(){
+    this.props.db.cards.find({_id:this.props.editingId}).then(
+      (data)=>{
+        this.setState({data:data.result[0]});
+        this._summary.value = data.result[0].summary || "";
+        this._desc.value = data.result[0].description || "";
+      }
+    )
+  },
+  componentDidMount:function(){
+    //if(this.state.data
+    //this._summary.value = this.state.data.summary;
+    //this._desc.value = this.state.data.description;
+  },
+  render:function(){
+    return (
+      <div>
+        <input type="textbox" placeholder="summary" ref={(n)=>{this._summary=n}}/>
+        <div>
+          <textarea placeholder="description" ref={(n)=>{this._desc=n}}/>
+        </div>
+        <button onClick={this.save}>Save</button>
       </div>
     )
   }
@@ -122,7 +299,10 @@ let Board = React.createClass({
 render((
   <div>
     <Router history={browserHistory}>
-      <Route path="/" component={Home}/>
+      <Route path="/" component={Home} db={db} client={baasClient}/>
+      <Route path="boards" db={db} component={Boards}>
+        <Route path=":id" component={Board} db={db}/>
+      </Route>
     </Router>
   </div>
 ), document.getElementById('app'))
