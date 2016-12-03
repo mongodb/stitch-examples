16642374fc0412a35448e3928e67611c54d14aca
Mike O'Brien <mpobrien005@gmail.com>
2016-12-03 15:49:27 -0500

    make todo webapp prettier

--- todo/src/index.js
+++ todo/src/index.js
@@ -16,12 +16,11 @@ let TodoItem = React.createClass({
 		.then(this.props.onChange)
 	},
 	render(){
-	console.log(this.props)
 		let itemClass = this.props.item.checked ? "done" : "";
 		return (
 			<div className="todo-item-root">
-				<label className="todo-item-container">
-					<div className={"checkbox-input " + itemClass} onClick={this.clicked}>
+				<label className="todo-item-container" onClick={this.clicked}>
+					<div className={"checkbox-input " + itemClass}>
 						{this.props.item.checked ? "✓" : null}
 					</div>
 					<span className={"todo-item-text " + itemClass}>{this.props.item.text}</span>
@@ -37,25 +36,38 @@ function TodoItem({item=null, checkHandler=null}){
 
 var AuthControls = React.createClass({
   render: function(){
+		console.log(baasClient.auth())
     let authed = this.props.client.auth() != null
     let logout = () => this.props.client.logout()
     return (
       <div>
-        { authed ? <div>Logged in via {baasClient.auth()['provider'].split("/")[1]} </div>: null }
+        { authed ? <div>Logged in as {JSON.stringify(baasClient.auth().data)} via {baasClient.auth()['provider'].split("/")[1]} </div>: null }
 				{	!authed ? 
 					<div>
-						<button onClick={() => this.props.client.authWithOAuth("google")}>Login with Google</button>
-						<button onClick={() => this.props.client.authWithOAuth("facebook")}>Login with Facebook</button>
+						<div onClick={() => this.props.client.authWithOAuth("google")} className="signin-button">
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 48 48">
+								<g>
+									<path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+									<path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+									<path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+									<path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+									<path fill="none" d="M0 0h48v48H0z"></path>
+								</g>
+							</svg>
+							<span className="signin-button-text">Sign in with Google</span>
+						</div>
+						<div onClick={() => this.props.client.authWithOAuth("facebook")} className="signin-button">
+              <div className="facebook-signin-logo"></div>
+							<span className="signin-button-text">Sign in with Facebook</span>
+            </div>
 					</div>
 					: null
 				}
-        <button disabled={!authed} onClick={() => this.props.client.logout()}>Logout</button>
+        {authed ?  <button onClick={() => this.props.client.logout()}>Logout</button> : null}
       </div>
     )
-        //<button disabled={authed}
-          //onClick={() => this.props.client.linkWithOAuth("google")}>Link with Google</button>
-        //<button disabled={authed}
-          //onClick={() => this.props.client.linkWithOAuth("facebook")}>Link with Facebook</button>
+		//<button disabled={authed} onClick={() => this.props.client.linkWithOAuth("google")}>Link with Google</button>
+		//<button disabled={authed} onClick={() => this.props.client.linkWithOAuth("facebook")}>Link with Facebook</button>
   },
 })
 
@@ -100,9 +112,14 @@ var TodoList = React.createClass({
   render: function(){
     let loggedInResult = 
       (<div>
-        <button onClick={this.clear}>Clean up</button>
-        <ul>
-        <input type="text" className="new-item" placeholder="add a new item..." ref={ (n)=>{this._newitem=n} } onKeyDown={this.addItem}/>
+				<div className="controls">
+        	<input type="text" className="new-item" placeholder="add a new item..." ref={ (n)=>{this._newitem=n} } onKeyDown={this.addItem}/>
+					{this.state.items.filter((x)=>x.checked).length > 0 ? 
+						<div  href="" className="cleanup-button" onClick={this.clear}>clean up</div>
+						: null 
+					}
+				</div>
+        <ul className="items-list">
         { 
           this.state.items.length == 0
           ?  <div>list is empty.</div>
@@ -122,8 +139,8 @@ var Home = function(){
     <div>
       {authed ? <Link to="/settings" className="settings-link">Settings</Link> : null}
       <div>
-        <TodoList/>
         <AuthControls client={baasClient}/>
+        <TodoList/>
       </div>
     </div>
   )
