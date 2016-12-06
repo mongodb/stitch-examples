dc7069bc357777ee51616594a5c60d07b91f752b
Eric Daniels <eric@erdaniels.com>
2016-12-06 14:17:39 -0500

    add envs to todo app

--- todo/webpack.config.js
+++ todo/webpack.config.js
@@ -1,8 +1,16 @@
+webpack = require('webpack')
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://0.0.0.0:8001', // WebpackDevServer host and port
     'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     './src/index.js'
+  ],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    })
   ],
   module: {
     loaders: [
