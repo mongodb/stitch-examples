c35971190e5a2ac583e28e8eb9ed13950e560ee4
rlondner <raphael+github@mongodb.com>
2017-08-17 15:15:00 -0400

    Removed "Bearer" prefix from YELP_AUTH_TOKEN
    Moved "Bearer" prefix to index.js to avoid confusion

--- PlateSpace/ETL/config.js
+++ PlateSpace/ETL/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGODB_ATLAS_URI: "<enter your MongoDB Atlas connection string here>",
-  YELP_AUTH_TOKEN: "Bearer [YOUR_YELP_BEARER_TOKEN]",
+  YELP_AUTH_TOKEN: "[YOUR_YELP_BEARER_TOKEN]",
   BASE_LOCATION_LATITUDE:37.7749,
   BASE_LOCATION_LONGITUDE:-122.4194,
   YELP_RESTAURANTS_REQ: "https://api.yelp.com/v3/businesses/search?term=food&limit=50"
