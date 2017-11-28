b20ff920ab040d5baa9efdcc1d96f2d81d613e36
Kevin Albertson <kevin.albertson@10gen.com>
2017-11-28 10:52:44 -0500

    Handle case where no weather data exists

--- IoTTemperatureTracking/twilio_webhook.js
+++ IoTTemperatureTracking/twilio_webhook.js
@@ -5,32 +5,27 @@ exports = function(message){
   //Define Services and Collections
   var mongodb = context.services.get("mongodb-atlas");
   var TempData = mongodb.db("Imp").collection("TempData");
-
   var twilio = context.services.get("twilio");
+  var responseMessage = "No weather data found";
 
   if(message.Body.toLowerCase() === "temp" || message.Body.toLowerCase() === "temp now"){
     var data = TempData.find().sort({"Timestamp" : -1}).limit(1).toArray();
-    //Send the response
-    return twilio.send({
-      from : context.values.get("TwilioPhone"),
-      to : message.From,
-      body : "As of " + data[0].Date + " the indoor temperature was " + data[0].indoorTemp + "°F and the outdoor temperature was " + data[0].outdoorTemp + "°F."});
-
+    if (data.length === 1){
+      responseMessage = "As of " + data[0].Date + " the indoor temperature was " + data[0].indoorTemp + "°F and the outdoor temperature was " + data[0].outdoorTemp + "°F.";
+    }
   }else if(!isNaN(userDate.valueOf())){
     var data = TempData.find({"Timestamp" : {"$lte" : userDate.getTime()/1000}}).sort({"Timestamp" : -1}).limit(1).toArray();
+    if (date.length === 1){
+      responseMessage = "As of " + data[0].Date + " the indoor temperature was " + data[0].indoorTemp + "°F and the outdoor temperature was " + data[0].outdoorTemp + "°F.";
+    }
+  }else{
+    responseMessage = "Invalid Request: Try \"Temp Now\" or \"Temp [Date]\"";
+  }
 
-    //Send the response
-    return twilio.send({
+  //Send the response
+  return twilio.send({
     from : context.values.get("TwilioPhone"),
     to : message.From,
-    body : "As of " + data[0].Date + " the indoor temperature was " + data[0].indoorTemp + "°F and the outdoor temperature was " + data[0].outdoorTemp + "°F."});
-
-  } else {
-    //Send the response
-    return twilio.send({
-      from : context.values.get("TwilioPhone"),
-      to : message.From,
-      body : "Invalid Request: Try \"Temp Now\" or \"Temp [Date]\""
-    });
-  }
+    body : responseMessage
+  });
 };
