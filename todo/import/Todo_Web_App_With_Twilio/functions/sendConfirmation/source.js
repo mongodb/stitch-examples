ed6dcb5658b56727b39d5342ee82a4155146e82e
MongoCaleb <caleb.thompson@mongodb.com>
2018-04-03 20:54:45 -0700

    remove unrelated files

--- todo/import/Todo_Web_App_With_Twilio/functions/sendConfirmation/source.js
+++
@@ -1,8 +0,0 @@
-exports = function(recipient, code) {
-    var twilio = context.services.get("tw1");
-    twilio.send({
-        from: context.values.get("ourNumber"),
-        to: recipient,
-        body: "Your confirmation code is " + code
-    });
-}
