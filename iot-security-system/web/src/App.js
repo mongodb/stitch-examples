d041e24d672c437357ac36b20b952a20b15fb65e
Eliot Horowitz <eliot@10gen.com>
2018-04-19 00:23:29 -0400

    settings and such things

--- iot-security-system/web/src/App.js
+++ iot-security-system/web/src/App.js
@@ -38,14 +38,17 @@ class App extends Component {
         this.client = props.client;
         this.db = this.client.service('mongodb', 'mongodb-atlas').db('security-system');
 
-        this.state = { doCapture: false, images: [] };
+        this.state = { doCapture: false, images: [], settings: { camera_url: '', unlock_url: '' } };
 
         this.startCapture = this.startCapture.bind(this);
         this.stopCapture = this.stopCapture.bind(this);
         this.loadImages = this.loadImages.bind(this);
         this.removeImage = this.removeImage.bind(this);
+        this.setCameraURL = this.setCameraURL.bind(this);
+        this.setUnlockURL = this.setUnlockURL.bind(this);
 
         this.loadImages();
+        this.loadSettings();
     }
 
     startCapture() {
@@ -78,6 +81,17 @@ class App extends Component {
             });
     }
 
+    loadSettings() {
+        this.db
+            .collection('settings')
+            .findOne({ _id: this.client.authedId() })
+            .then(settings => {
+                if (settings) {
+                    this.setState({ settings: settings });
+                }
+            });
+    }
+
     removeImage(id) {
         let q = { _id: id };
         this.db
@@ -86,7 +100,22 @@ class App extends Component {
             .then(this.loadImages);
     }
 
+    setCameraURL() {
+        var newUrl = document.getElementById('camera_url').value;
+        this.db
+            .collection('settings')
+            .updateOne({ _id: this.client.authedId() }, { $set: { camera_url: newUrl } }, { upsert: true });
+    }
+
+    setUnlockURL() {
+        var newUrl = document.getElementById('unlock_url').value;
+        this.db
+            .collection('settings')
+            .updateOne({ _id: this.client.authedId() }, { $set: { unlock_url: newUrl } }, { upsert: true });
+    }
+
     render() {
+        const extraButtonPieces = 'buttonId={{ButtonId}}&name={{Name}}&battery={{Battery}}';
         return (
             <div className="App">
                 <header className="App-header">
@@ -103,6 +132,22 @@ class App extends Component {
                 </div>
                 <button onClick={this.startCapture}>Add new picture</button>
                 <WebcamCapture active={this.state.doCapture} stopCapture={this.stopCapture} />
+                <div>
+                    <div>Button Pushed Url</div>
+                    <span className="displayurl">
+                        https://webhooks.mongodb-stitch.com/api/client/v2.0/app/{this.client.clientAppID}/service/http/incoming_webhook/button_pressed?secret=abc&user={this.client.authedId()}&{
+                            extraButtonPieces
+                        }
+                    </span>
+
+                    <div>Camera Image URL</div>
+                    <input id="camera_url" value={this.state.settings.camera_url} />
+                    <button onClick={this.setCameraURL}>Set Camera URL</button>
+
+                    <div>Unlock URL</div>
+                    <input id="unlock_url" value={this.state.settings.unlock_url} />
+                    <button onClick={this.setUnlockURL}>Set UnLock URL</button>
+                </div>
             </div>
         );
     }
--- iot-security-system/web/src/index.js
+++ iot-security-system/web/src/index.js
@@ -12,7 +12,6 @@ async function startup() {
         await client.authenticate('google');
         return;
     }
-    console.log(client.authedId());
 
     ReactDOM.render(<App client={client} />, document.getElementById('root'));
     registerServiceWorker();
